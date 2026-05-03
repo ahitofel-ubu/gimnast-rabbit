4ca9e74ad3d55bfa884dacee67b55470b5eb5e85
Your Name <s268842@studenti.polito.it>
2026-05-03 22:40:01 +0200

    1.0.0

--- script.js
+++ script.js
@@ -1,58 +1,84 @@
 
 document.querySelectorAll(".nav-item").forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelectorAll(".nav-item").forEach(b => b.classList.remove("active"));
-        btn.classList.add("active");
-    });
+  btn.addEventListener("click", () => {
+    document.querySelectorAll(".nav-item").forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
 });
 
-window.addEventListener("scroll", () => {
-    const trigger = 150;
-    const bg = document.querySelector(".bg");
-    if (window.scrollY > trigger) {
-        bg.style.opacity = "0.4";
-    } else {
-        bg.style.opacity = "0";
-    }
+
+/* const tl = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
+tl.from("header img", {
+  y: -50,
+  opacity: 0,
+  duration: 1
+})
+  .from(".title", {
+    y: 50,
+    opacity: 0,
+    duration: 1
+  }, "-=0.5")
+  .from(".hero-content h1", {
+    y: 50,
+    opacity: 0,
+    duration: 1
+  }, "-=0.5")
+  .from(".hero-content h3, .hero-content p", {
+    y: 30,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 0.8
+  });
+ */
+
+gsap.to("#navbar", {
+  scrollTrigger: {
+    trigger: "body",
+    start: "top -100",
+    toggleActions: "play none none reverse"
+  },
+  scale: 0.95,
+  duration: 0.3
 });
 
-let ticking = false;
-let offset = 0;
-window.addEventListener("scroll", () => {
-    if (!ticking) {
-        window.requestAnimationFrame(() => {
+gsap.to(".bg", {
+  scrollTrigger: {
+    trigger: "body",
+    start: "top -100",
+    toggleActions: "play none none reverse"
+  },
+  opacity: 0.1,
+  duration: 1
+});
 
-            const scroll = window.scrollY * 0.8;
-            
+gsap.to(".bg", {
+  scrollTrigger: {
+    trigger: "body",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 3
+  },
+  y: -500,
+  scale: 1.5,
+  duration: 1
+});
+gsap.to(".title", {
+  color: "#bb1e10",
+  duration: 2.5,
+  repeat: -1,
+  yoyo: true,
+  ease: "power3.in",
+});
+gsap.to(".title", {
+  height: "200px",
+  scrollTrigger: {
+    trigger: "body",
+    start: "top top",
+    end: "400px top",
+    scrub: 1
+  },
+  y: 100,
 
-            // 🌄 background lento + zoom dinamico
-            const bg = document.querySelector(".bg");
-            const y = (scroll - offset) * 0.2;
-            const s = 0.8 + (scroll - offset) * 0.0005;
-
-            bg.style.transform = `translateY(${y}px) scale(${s})`;
-            if (scroll > 1200) {
-                offset = 1200;
-                bg.style.backgroundImage = "url('bg2.jpg')";
-/*             } else if (scroll > 2500) {
-                offset = 2500;
-                bg.style.backgroundImage = "url('bg3.jpg')"; */
-            }else {
-                offset = 0;
-                bg.style.backgroundImage = "url('bg1.jpg')";
-            }
-
-
-            // ✨ fade out testo hero
-            const hero = document.querySelector(".hero-content");
-            hero.style.opacity = 1 - scroll / 900;
-
-            hero.style.transform =
-                `translateY(${scroll * 0.8}px)`;
-
-            ticking = false;
-        });
-
-        ticking = true;
-    }
+  duration: 0.5
 });
